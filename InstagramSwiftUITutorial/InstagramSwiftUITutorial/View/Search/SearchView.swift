8ef284c8ed231992a53564037da52b49bf730433
dayo2n <0217dayun@naver.com>
2022-03-08 17:01:09 +0900

    docs: apply .gitignore

---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorial/View/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViw.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by 문다 on 2022/03/08.
+//
+
+import SwiftUI
+
+struct SearchViw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchViw_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchViw()
+    }
+}
