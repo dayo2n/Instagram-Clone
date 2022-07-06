6d8af6756241ddda49e305923879750fd45a9c84
dayo2n <0217dayun@naver.com>
2022-07-06 17:27:06 +0900

    [feat] create Notification Model

---
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorial/Model/Notification.swift
@@ -0,0 +1,31 @@
+//
+//  Notification.swift
+//  InstagramSwiftUITutorial
+//
+//  Created by 문다 on 2022/07/06.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var postId: String?
+    let username: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+    case like, comment, follow // 자동으로 0부터 인덱스 부여
+    
+    var notificationMessage: String {
+        switch self {
+        case .like: return " liked one of your posts."
+        case .comment: return " commented on one of your posts."
+        case .follow: return " started following you."
+        }
+    }
+}
