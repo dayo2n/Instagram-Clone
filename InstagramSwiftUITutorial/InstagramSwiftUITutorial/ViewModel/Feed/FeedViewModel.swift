9d8d130821df53712be9fcc547b2749214d4ead3
dayo2n <0217dayun@naver.com>
2022-07-07 17:35:13 +0900

    [feat] feed를 업로드한 최신순으로 정렬

--- InstagramSwiftUITutorial/InstagramSwiftUITutorial/ViewModel/Feed/FeedViewModel.swift
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorial/ViewModel/Feed/FeedViewModel.swift
@@ -15,11 +15,9 @@ class FeedViewModel: ObservableObject {
     }
     
     func fetchPosts() {
-        COLLECTION_POSTS.getDocuments { snapshot, _ in
+        COLLECTION_POSTS.order(by: "timestamp", descending: true).getDocuments { snapshot, _ in
             guard let documents = snapshot?.documents else { return }
             self.posts = documents.compactMap({ try? $0.data(as: Post.self) })
-            
-            print(self.posts)
         }
     }
 }
