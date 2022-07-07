da6f84d78f0910007ac6c43fe854be562dc0389c
dayo2n <0217dayun@naver.com>
2022-07-07 17:39:54 +0900

    [feat] postGridView에서 포스트를 최신순으로 정렬

--- InstagramSwiftUITutorial/InstagramSwiftUITutorial/ViewModel/PostGridViewModel.swift
+++ InstagramSwiftUITutorial/InstagramSwiftUITutorial/ViewModel/PostGridViewModel.swift
@@ -42,7 +42,8 @@ class PostGridViewModel: ObservableObject {
     func fetchUserPosts(forUid uid : String) {
         COLLECTION_POSTS.whereField("ownerUid", isEqualTo: uid).getDocuments { snapshot, _ in
             guard let documents = snapshot?.documents else { return }
-            self.posts = documents.compactMap({ try? $0.data(as: Post.self) })
+            let posts = documents.compactMap({ try? $0.data(as: Post.self) })
+            self.posts = posts.sorted(by: { $0.timestamp.dateValue() > $1.timestamp.dateValue() })
         }
     }
 }
